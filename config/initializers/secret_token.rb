ef62f391987a81a7b607fb47c21c7ce58b4828c6
Ashley Rae Lee <student@epicodus.com>
2014-04-10 18:42:33 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EpiCoderNotes::Application.config.secret_key_base = '7b80e34cc76ba5506d69901cbc3b9770529b236a7b854e5a8dee5ea27886194d20b0a123a9a3d0d9a3f83efa0bb8a90c03384c65103eec2815779f71fa5f9096'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EpiCoderNotes::Application.config.session_store :cookie_store, key: '_EpiCoderNotes_session'
