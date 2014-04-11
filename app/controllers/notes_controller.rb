eb60c91c11f3342fd50b0e1f8433822ec523cf77
Ashley Rae Lee <student@epicodus.com>
2014-04-11 16:06:53 -0700

    updates controller and index form

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,17 +8,21 @@ class NotesController < ApplicationController
     @note = Note.new
   end
 
+  def show
+    @note = Note.find(params[:id])
+  end
+
   def create
     @note = Note.new(note_params)
     if @note.save
-      redirect_to root_url, notice: "Thank you for Adding Notes!"
+      redirect_to '/notes', notice: "Thank you for Adding Notes!"
     else
       render "new"
     end
   end
 
   def note_params
-
+    params.require(:note).permit(:title, :content, :tags_id, :author_id)
   end
 
 end
